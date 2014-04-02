60a87a96738f12d55c1ff5723e7281617da68a07
stefanoantonel <stefanoantonel@gmail.com>
2014-04-02 11:37:53 -0300

    en url d redirecciona a otro lado

--- Chieti/proj2/tests.py
+++ Chieti/proj2/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from django.http import request, HttpResponse
 # Create your tests here.
+def index(request):
+    p = request.GET.get('p')
+    if p is not None:
+        return HttpResponse(p)
+    else:
+        return HttpResponse("No hay p ")
+
